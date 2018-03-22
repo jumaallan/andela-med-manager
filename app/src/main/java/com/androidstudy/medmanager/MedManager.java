fc58ff2e39cae5bc49247683f20d80feb9659b19
Juma Allan <allanjuma@gmail.com>
2018-03-22 11:15:33 +0300

    Added packages - data, di, ui and util

---
+++ app/src/main/java/com/androidstudy/medmanager/MedManager.java
@@ -0,0 +1,7 @@
+package com.androidstudy.medmanager;
+
+import android.app.Application;
+
+
+public class MedManager extends Application {
+}
--- app/src/main/java/com/androidstudy/medmanager/ui/MainActivity.java
+++ app/src/main/java/com/androidstudy/medmanager/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.androidstudy.medmanager;
+package com.androidstudy.medmanager.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.androidstudy.medmanager.R;
 
 public class MainActivity extends AppCompatActivity {
 
