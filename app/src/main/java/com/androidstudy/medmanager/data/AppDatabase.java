550865582e0feac65d5cb8915b95e81b7076fd0f
Juma Allan <allanjuma@gmail.com>
2018-03-22 22:25:15 +0300

    Added Medicine ViewModel

--- app/src/main/java/com/androidstudy/medmanager/data/AppDatabase.java
+++ app/src/main/java/com/androidstudy/medmanager/data/AppDatabase.java
@@ -1,29 +1,29 @@
 package com.androidstudy.medmanager.data;
 
+import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.arch.persistence.room.TypeConverters;
 import android.content.Context;
 
+import com.androidstudy.medmanager.data.dao.MedicineDao;
+import com.androidstudy.medmanager.data.model.Medicine;
+import com.androidstudy.medmanager.data.model.User;
+
+@Database(entities = {Medicine.class, User.class}, version = 1)
 @TypeConverters(DateConverter.class)
 public abstract class AppDatabase extends RoomDatabase {
 
-    private static final String DB_NAME = "medManager.db";
-    private static volatile AppDatabase instance;
+    private static AppDatabase INSTANCE;
 
-    static synchronized AppDatabase getInstance(Context context) {
-        if (instance == null) {
-            instance = create(context);
+    public static AppDatabase getDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE =
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "medmanager_db")
+                            .build();
         }
-        return instance;
+        return INSTANCE;
     }
 
-    private static AppDatabase create(final Context context) {
-        return Room.databaseBuilder(
-                context,
-                AppDatabase.class,
-                DB_NAME).build();
-    }
-
-    public abstract AppDatabase getRepoDao();
+    public abstract MedicineDao medicineDao();
 }
--- app/src/main/java/com/androidstudy/medmanager/data/dao/MedicineDao.java
+++ app/src/main/java/com/androidstudy/medmanager/data/dao/MedicineDao.java
@@ -2,6 +2,7 @@ package com.androidstudy.medmanager.data.dao;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
 
@@ -20,4 +21,7 @@ public interface MedicineDao {
 
     @Insert(onConflict = REPLACE)
     void addMedicine(Medicine medicine);
+
+    @Delete
+    void deleteMedicine(Medicine medicine);
 }
