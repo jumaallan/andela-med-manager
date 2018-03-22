8e99e19b3436ae932f90cf636fbf6b56e60a7af6
Juma Allan <allanjuma@gmail.com>
2018-03-22 12:51:59 +0300

    Added Timber and Medicine Activities

---
+++ app/src/main/java/com/androidstudy/medmanager/ui/ui/medicine/AddMedicineActivity.java
@@ -0,0 +1,15 @@
+package com.androidstudy.medmanager.ui.ui.medicine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.androidstudy.medmanager.R;
+
+public class AddMedicineActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_medicine);
+    }
+}
---
+++ app/src/main/java/com/androidstudy/medmanager/ui/ui/medicine/MedicineSuccessActivity.java
@@ -0,0 +1,15 @@
+package com.androidstudy.medmanager.ui.ui.medicine;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.androidstudy.medmanager.R;
+
+public class MedicineSuccessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine_success);
+    }
+}
