9e950e86d63a3aee06bd7c14f785c755273aba24
Juma Allan <allanjuma@gmail.com>
2018-03-22 12:54:15 +0300

    Introduced Lambda :) :)

--- app/src/main/java/com/androidstudy/medmanager/ui/ui/MainActivity.java
+++ app/src/main/java/com/androidstudy/medmanager/ui/ui/MainActivity.java
@@ -7,7 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.androidstudy.medmanager.R;
 
@@ -21,13 +20,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show());
     }
 
     @Override
