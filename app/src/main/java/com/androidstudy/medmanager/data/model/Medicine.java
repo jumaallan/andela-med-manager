ca2766dea6054c8203484eaa644521533f4293a7
Juma Allan <allanjuma@gmail.com>
2018-03-22 23:29:01 +0300

    Working Add Medicine :)

--- app/src/main/java/com/androidstudy/medmanager/data/model/Medicine.java
+++ app/src/main/java/com/androidstudy/medmanager/data/model/Medicine.java
@@ -5,7 +5,8 @@ import android.arch.persistence.room.PrimaryKey;
 
 @Entity
 public class Medicine {
-    @PrimaryKey(autoGenerate = true)u
+    @PrimaryKey(autoGenerate = true)
+    private long medicineId;
     // private long userId;
     private String name;
     private String description;
