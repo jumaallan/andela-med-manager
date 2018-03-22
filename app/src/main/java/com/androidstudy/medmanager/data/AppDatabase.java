206f70b52fc3d031ac5268144b54acee6578ace9
Juma Allan <allanjuma@gmail.com>
2018-03-22 12:09:23 +0300

    Added Room Database Setup

---
+++ app/src/main/java/com/androidstudy/medmanager/data/AppDatabase.java
@@ -0,0 +1,33 @@
+package com.androidstudy.medmanager.data;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+import com.androidstudy.medmanager.data.model.Medicine;
+import com.androidstudy.medmanager.data.model.User;
+
+
+@Database(entities = {User.class, Medicine.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static final String DB_NAME = "medManager.db";
+    private static volatile AppDatabase instance;
+
+    static synchronized AppDatabase getInstance(Context context) {
+        if (instance == null) {
+            instance = create(context);
+        }
+        return instance;
+    }
+
+    private static AppDatabase create(final Context context) {
+        return Room.databaseBuilder(
+                context,
+                AppDatabase.class,
+                DB_NAME).build();
+    }
+
+    public abstract AppDatabase getRepoDao();
+}
