124324877678690f5a8996fbce8eacd0407f6cc2
Juma Allan <allanjuma@gmail.com>
2018-03-22 11:28:40 +0300

    Setting up the MVVM Architecture :)

--- app/src/main/java/com/androidstudy/medmanager/ui/ui/MainActivity.java
+++ app/src/main/java/com/androidstudy/medmanager/ui/ui/MainActivity.java
@@ -1,4 +1,4 @@
-package com.androidstudy.medmanager.ui;
+package com.androidstudy.medmanager.ui.ui;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -17,10 +17,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
