b4c63185d059c224f5a1ef8ef8c76cf97fd0b447
Juma Allan <allanjuma@gmail.com>
2018-03-22 12:56:28 +0300

    Added Internet Permissions

--- app/src/main/java/com/androidstudy/medmanager/ui/ui/MainActivity.java
+++ app/src/main/java/com/androidstudy/medmanager/ui/ui/MainActivity.java
@@ -1,14 +1,15 @@
 package com.androidstudy.medmanager.ui.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import com.androidstudy.medmanager.R;
+import com.androidstudy.medmanager.ui.ui.medicine.AddMedicineActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,8 +21,10 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());
+        fab.setOnClickListener(view -> {
+            Intent medicine = new Intent(getApplicationContext(), AddMedicineActivity.class);
+            startActivity(medicine);
+        });
     }
 
     @Override
@@ -45,4 +48,8 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    //TODO :: Remove this later :)
+    //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
 }
