c740212848419f0ea7fd6663f985543b2e5747d4
Juma Allan <allanjuma@gmail.com>
2018-03-25 14:25:55 +0300

    Added Main Dashboard Adapter

---
+++ app/src/main/java/com/androidstudy/medmanager/data/model/MenuView.java
@@ -0,0 +1,37 @@
+package com.androidstudy.medmanager.data.model;
+
+public class MenuView {
+    private int id;
+    private String name;
+    private int image;
+
+    public MenuView(int id, String name, int image) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
---
+++ app/src/main/java/com/androidstudy/medmanager/ui/adapter/CustomItemClickListener.java
@@ -0,0 +1,7 @@
+package com.androidstudy.medmanager.ui.adapter;
+
+import android.view.View;
+
+public interface CustomItemClickListener {
+    public void onItemClick(View v, int position);
+}
