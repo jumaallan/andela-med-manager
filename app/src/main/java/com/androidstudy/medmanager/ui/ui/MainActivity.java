5392973b25ce15ad7fcc361083a74583b8d5b6c2
Juma Allan <allanjuma@gmail.com>
2018-03-27 21:57:13 +0300

    Code cleanup

--- app/src/main/java/com/androidstudy/medmanager/ui/ui/MainActivity.java
+++ app/src/main/java/com/androidstudy/medmanager/ui/ui/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ButterKnife.bind(this);
