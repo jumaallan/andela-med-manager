8aa00eca8b6dcf6eebebb4375fe6791de5ab93b2
Juma Allan <allanjuma@gmail.com>
2018-03-28 22:22:03 +0300

    A little housekeeping

--- app/src/main/java/com/androidstudy/medmanager/ThemableActivity.java
+++ app/src/main/java/com/androidstudy/medmanager/ThemableActivity.java
@@ -8,26 +8,26 @@ import com.androidstudy.medmanager.util.Settings;
 import timber.log.Timber;
 
 public class ThemableActivity extends BaseActivity {
-    public static final int[] themes = {R.style.AppTheme, R.style.AppTheme_Light};
-    private int themeIndex = Settings.themeIndex();
-
-    @Override
-    protected void onApplyThemeResource(Resources.Theme theme, int resid, boolean first) {
-        super.onApplyThemeResource(theme, getThemeRes(themeIndex), first);
-    }
-
-    @Override
-    protected boolean shouldRestart() {
-        return themeIndex != Settings.themeIndex();
-    }
-
-    @StyleRes
-    protected int getThemeRes(int index) {
-        try {
-            return themes[index];
-        } catch (Exception e) {
-            Timber.e(e);
-            return themes[0];
-        }
-    }
+    //public static final int[] themes = {R.style.AppTheme, R.style.AppTheme_Light};
+//    private int themeIndex = Settings.themeIndex();
+//
+//    @Override
+//    protected void onApplyThemeResource(Resources.Theme theme, int resid, boolean first) {
+//        super.onApplyThemeResource(theme, getThemeRes(themeIndex), first);
+//    }
+//
+//    @Override
+//    protected boolean shouldRestart() {
+//        return themeIndex != Settings.themeIndex();
+//    }
+//
+//    @StyleRes
+//    protected int getThemeRes(int index) {
+//        try {
+//            return themes[index];
+//        } catch (Exception e) {
+//            Timber.e(e);
+//            return themes[0];
+//        }
+//    }
 }
