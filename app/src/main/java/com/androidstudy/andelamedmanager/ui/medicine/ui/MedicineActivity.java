7f183bc4cba7d75e8bb30783b5e8747b6c8d059e
Juma Allan <allanjuma@gmail.com>
2018-03-30 19:13:17 +0300

    Added Medicine Profile Page

---
+++ app/src/main/java/com/androidstudy/andelamedmanager/ui/medicine/ui/MedicineActivity.java
@@ -0,0 +1,15 @@
+package com.androidstudy.andelamedmanager.ui.medicine.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androidstudy.andelamedmanager.base.ThemableActivity;
+
+public class MedicineActivity extends ThemableActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medicine);
+    }
+}
