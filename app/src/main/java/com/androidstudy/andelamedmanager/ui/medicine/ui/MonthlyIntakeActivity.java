879c3aa9768b914b1b1911d7e3d6b1872fa09598
Juma Allan <allanjuma@gmail.com>
2018-04-02 15:36:18 +0300

    Montly Intake

---
+++ app/src/main/java/com/androidstudy/andelamedmanager/ui/medicine/ui/MonthlyIntakeActivity.java
@@ -0,0 +1,15 @@
+package com.androidstudy.andelamedmanager.ui.medicine.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androidstudy.andelamedmanager.R;
+
+public class MonthlyIntakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_monthly_intake);
+    }
+}
