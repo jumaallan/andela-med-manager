8ab215a8cb24b99b04b4048ea72a42886ae97038
Juma Allan <allanjuma@gmail.com>
2018-04-02 15:47:22 +0300

    Updates - Toolbar

--- app/src/main/java/com/androidstudy/andelamedmanager/ui/medicine/ui/MonthlyIntakeActivity.java
+++ app/src/main/java/com/androidstudy/andelamedmanager/ui/medicine/ui/MonthlyIntakeActivity.java
@@ -1,15 +1,31 @@
 package com.androidstudy.andelamedmanager.ui.medicine.ui;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.androidstudy.andelamedmanager.R;
 import com.androidstudy.andelamedmanager.base.ThemableActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MonthlyIntakeActivity extends ThemableActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monthly_intake);
+        ButterKnife.bind(this);
+
+        Drawable upArrow = getResources().getDrawable(R.drawable.ic_chevron_left_white_24dp);
+        toolbar.setNavigationIcon(upArrow);
+        setSupportActionBar(toolbar);
+        setTitle(getResources().getString(R.string.activity_monthly_intake));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
--- app/src/main/java/com/androidstudy/andelamedmanager/ui/reminders/ui/ReminderActivity.java
+++ app/src/main/java/com/androidstudy/andelamedmanager/ui/reminders/ui/ReminderActivity.java
@@ -1,15 +1,31 @@
 package com.androidstudy.andelamedmanager.ui.reminders.ui;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.androidstudy.andelamedmanager.R;
 import com.androidstudy.andelamedmanager.base.ThemableActivity;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ReminderActivity extends ThemableActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reminder);
+        ButterKnife.bind(this);
+
+        Drawable upArrow = getResources().getDrawable(R.drawable.ic_chevron_left_white_24dp);
+        toolbar.setNavigationIcon(upArrow);
+        setSupportActionBar(toolbar);
+        setTitle(getResources().getString(R.string.activity_reminders));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
 }
