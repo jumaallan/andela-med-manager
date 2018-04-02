8baa7d161f2c4f3200de72c3ca1566031345dfc8
Juma Allan <allanjuma@gmail.com>
2018-04-02 11:12:40 +0300

    Reworked the AlarmReceiver

--- app/src/main/java/com/androidstudy/andelamedmanager/notifications/AlarmReceiver.java
+++ app/src/main/java/com/androidstudy/andelamedmanager/notifications/AlarmReceiver.java
@@ -1,4 +1,50 @@
 package com.androidstudy.andelamedmanager.notifications;
 
-public class AlarmReceiver {
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+
+import com.androidstudy.andelamedmanager.R;
+import com.androidstudy.andelamedmanager.ui.main.ui.MainActivity;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private static final int NOTIFICATION_ID = 0;
+
+
+    public AlarmReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        //Create the content intent for the notification, which launches this activity
+        Intent contentIntent = new Intent(context, MainActivity.class);
+        PendingIntent contentPendingIntent = PendingIntent.getActivity
+                (context, NOTIFICATION_ID, contentIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+
+        //Build the notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.mipmap.ic_launcher_andela_round)
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(context.getString(R.string.notification_text))
+                .setContentIntent(contentPendingIntent)
+                .setAutoCancel(true)
+                .setSound(alarmSound)
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setDefaults(NotificationCompat.DEFAULT_ALL);
+
+        //Deliver the notification
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
 }
