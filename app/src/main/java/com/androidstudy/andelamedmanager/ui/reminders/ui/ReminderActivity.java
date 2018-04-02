4f32f2ff42060b940486b939611b48395b082932
Juma Allan <allanjuma@gmail.com>
2018-04-02 15:37:26 +0300

    Reminder Activity

---
+++ app/src/main/java/com/androidstudy/andelamedmanager/ui/reminders/ui/ReminderActivity.java
@@ -0,0 +1,15 @@
+package com.androidstudy.andelamedmanager.ui.reminders.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.androidstudy.andelamedmanager.R;
+
+public class ReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder);
+    }
+}
