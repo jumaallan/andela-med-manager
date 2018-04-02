a7bdd916c52a9f340d27369f369973f00a62f45a
Juma Allan <allanjuma@gmail.com>
2018-04-02 10:49:58 +0300

    Added Notification Schedule

---
+++ app/src/main/java/com/androidstudy/andelamedmanager/alarm/AlarmReceiver.java
@@ -0,0 +1,30 @@
+package com.androidstudy.andelamedmanager.alarm;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+import com.androidstudy.andelamedmanager.ui.main.ui.MainActivity;
+
+public class AlarmReceiver extends BroadcastReceiver {
+
+    String TAG = "AlarmReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction() != null && context != null) {
+            if (intent.getAction().equalsIgnoreCase(Intent.ACTION_BOOT_COMPLETED)) {
+                // Set the alarm here.
+                Log.d(TAG, "onReceive: BOOT_COMPLETED");
+                LocalData localData = new LocalData(context);
+                NotificationScheduler.setReminder(context, AlarmReceiver.class,
+                        localData.get_hour(), localData.get_min());
+                return;
+            }
+        }
+        //Trigger the notification
+        NotificationScheduler.showNotification(context, MainActivity.class,
+                "You have 5 unwatched videos", "Watch them now?");
+    }
+}
