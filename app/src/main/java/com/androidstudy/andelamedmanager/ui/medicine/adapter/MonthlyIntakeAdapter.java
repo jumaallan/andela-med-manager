6b6c120d8c382a0eac4be01a623d9b4ed05ef936
Juma Allan <allanjuma@gmail.com>
2018-04-03 22:49:12 +0300

    Worked on Medicine Search Option

---
+++ app/src/main/java/com/androidstudy/andelamedmanager/ui/medicine/adapter/MonthlyIntakeAdapter.java
@@ -0,0 +1,129 @@
+package com.androidstudy.andelamedmanager.ui.medicine.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+
+import com.androidstudy.andelamedmanager.R;
+import com.androidstudy.andelamedmanager.data.model.Medicine;
+import com.androidstudy.andelamedmanager.ui.main.adapter.CustomItemClickListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MonthlyIntakeAdapter extends RecyclerView.Adapter<MonthlyIntakeAdapter.MedicineViewHolder> implements Filterable {
+    CustomItemClickListener listener;
+    private Context context;
+    private List<Medicine> medicineList;
+    private List<Medicine> medicineListFiltered;
+
+    public MonthlyIntakeAdapter(Context context, List<Medicine> medicineList, CustomItemClickListener listener) {
+        this.context = context;
+        this.medicineList = medicineList;
+        this.medicineListFiltered = medicineList;
+        this.listener = listener;
+    }
+
+    @Override
+    public MedicineViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.row_monthly_intake, parent, false);
+        final MedicineViewHolder mViewHolder = new MedicineViewHolder(itemView);
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                listener.onItemClick(v, mViewHolder.getPosition());
+            }
+        });
+        return mViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(final MedicineViewHolder holder, final int position) {
+        final Medicine medicine = medicineListFiltered.get(position);
+
+        holder.textViewMedicineName.setText(medicine.getName());
+        holder.textViewMedicineInterval.setText(medicine.getInterval());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return medicineListFiltered.size();
+    }
+
+    /**
+     * Remove all elements from the list.
+     */
+    public void clear() {
+        final int size = getItemCount();
+        medicineList.clear();
+        medicineListFiltered.clear();
+        notifyItemRangeRemoved(0, size);
+    }
+
+    public void setItems(List<Medicine> items) {
+        this.clear();
+        this.medicineList = items;
+        this.medicineListFiltered = items;
+        this.notifyDataSetChanged();
+    }
+
+    //method filter
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    medicineListFiltered = medicineList;
+                } else {
+                    List<Medicine> filteredList = new ArrayList<>();
+                    for (Medicine row : medicineList) {
+
+                        // name match condition. this might differ depending on your requirement
+                        // here we are looking for name or phone number match
+                        if (row.getName().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+                    medicineListFiltered = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = medicineListFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                medicineListFiltered = (ArrayList<Medicine>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    class MedicineViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.textViewMedicineName)
+        TextView textViewMedicineName;
+        @BindView(R.id.textViewMedicineInterval)
+        TextView textViewMedicineInterval;
+        @BindView(R.id.textViewMedicinePercentage)
+        TextView textViewMedicinePercentage;
+
+        MedicineViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+}
--- app/src/main/java/com/androidstudy/andelamedmanager/ui/medicine/ui/MonthlyIntakeActivity.java
+++ app/src/main/java/com/androidstudy/andelamedmanager/ui/medicine/ui/MonthlyIntakeActivity.java
@@ -1,11 +1,24 @@
 package com.androidstudy.andelamedmanager.ui.medicine.ui;
 
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.SearchView;
 
 import com.androidstudy.andelamedmanager.R;
 import com.androidstudy.andelamedmanager.base.ThemableActivity;
+import com.androidstudy.andelamedmanager.data.model.Medicine;
+import com.androidstudy.andelamedmanager.ui.main.adapter.CustomItemClickListener;
+import com.androidstudy.andelamedmanager.ui.medicine.adapter.MonthlyIntakeAdapter;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -14,6 +27,14 @@ public class MonthlyIntakeActivity extends ThemableActivity {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.layout_empty)
+    FrameLayout layout_empty;
+    @BindView(R.id.searchViewMedicine)
+    SearchView searchViewMedicine;
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    MonthlyIntakeAdapter monthlyIntakeAdapter;
+    private List<Medicine> medicineList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +48,8 @@ public class MonthlyIntakeActivity extends ThemableActivity {
         setTitle(getResources().getString(R.string.activity_monthly_intake));
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        loadSearch();
     }
 
     @Override
@@ -34,5 +57,62 @@ public class MonthlyIntakeActivity extends ThemableActivity {
         finish();
         onBackPressed();
         return true;
+    }
+
+    public void setListData(final List<Medicine> medicineList) {
+        this.medicineList = medicineList;
+
+        if (medicineList.isEmpty()) {
+            layout_empty.setVisibility(View.VISIBLE);
+        }
+
+        monthlyIntakeAdapter = new MonthlyIntakeAdapter(this, medicineList, new CustomItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position) {
+                Medicine medicine = medicineList.get(position);
+                Intent intent = new Intent(getApplicationContext(), MedicineActivity.class);
+                Bundle b = new Bundle();
+
+                b.putString("name", medicine.getName());
+                b.putString("description", medicine.getDescription());
+                b.putString("interval", medicine.getInterval());
+                b.putString("pills", medicine.getPills());
+                b.putString("pillsTaken", medicine.getPillsTaken());
+                b.putBoolean("true", medicine.isHasNotification());
+                b.putString("startDate", medicine.getStartDate());
+                b.putString("endDate", medicine.getEndDate());
+                b.putInt("days", medicine.getDays());
+                intent.putExtras(b);
+                startActivity(intent);
+
+            }
+        });
+        LinearLayoutManager mLayoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.addItemDecoration(
+                new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(monthlyIntakeAdapter);
+    }
+
+    /**
+     * Setup search view.
+     */
+    private void loadSearch() {
+        searchViewMedicine.setFocusable(false);
+        //adding search listener
+        searchViewMedicine.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                monthlyIntakeAdapter.getFilter().filter(query);
+                return false;
+            }
+        });
     }
 }
