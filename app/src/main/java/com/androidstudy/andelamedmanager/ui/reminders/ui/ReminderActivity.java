243ccf732fc440db3000f348d530ce2f0c58099f
Juma Allan <allanjuma@gmail.com>
2018-04-06 12:51:13 +0300

    A little housekeeping

--- app/src/main/java/com/androidstudy/andelamedmanager/ui/reminders/ui/ReminderActivity.java
+++ app/src/main/java/com/androidstudy/andelamedmanager/ui/reminders/ui/ReminderActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
+import com.androidstudy.andelamedmanager.R;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
